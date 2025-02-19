009e5393deeac5e236bd8349c84a5502ed5a02d6
potatotomato22206 <22206@wc.school.nz>
2025-02-19 15:02:38 +1300

    Add files via upload
    Replaced error detection.
    
    Created Exit option post test.
    
    Commented code

--- Quick Function Quiz.py
+++ Quick Function Quiz.py
@@ -7,26 +7,27 @@ QuestionStore = [
 ]
 AnswerStore = ['C', 'B', 'D', 'B', 'C']
 
-def Quizmaster():
+def Quizmaster():#This Reads the question index, prints the first question, asks for an answer, if the answer is inline with the correct answer index then grants a point.
     Score = 0
-    for i in range(len(QuestionStore)):
+    for i in range(len(QuestionStore)): #Print the first question, followed by the next questions until it runs out of questions
         print(QuestionStore[i])
-        
-        try:
-            if input().strip().upper() == AnswerStore[i].upper():
+        GoodAnswer = False  #Set the while error detection var to false
+        while GoodAnswer == False:
+            inputt = input().strip().upper() #Aquire input answer
+            if inputt == AnswerStore[i].upper(): #If the answer is the same return correct
                 Score += 1
                 print(f'Correct! current score = {Score}')
-                
-            else: print('Incorrect, Sorry.')
-            
-        except:
-            print("Invalid Answer")
-
+                GoodAnswer = True     #Exit error detection
+            elif inputt == ("A" or "B" or "C" or "D"):  #If answer is incorrect, but one of the options, return incorrect
+                print('Incorrect, Sorry.')
+                GoodAnswer = True       #exit error detection
+            else:
+                print("Invalid Answer, Try Again") #answer wasn't in the list or correct, try answer again.
     print()
     print()
-    print(f'Your final score was {Score}!')
+    print(f'Your final score was {Score}!')  #Ran out of questions
     
-
-
-    
-Quizmaster()
+while True:
+    Quizmaster()  #Run quizmaster
+    if input("X to exit      ").upper() == "X": #If x is inputted quit
+        quit()
